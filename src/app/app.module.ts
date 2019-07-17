261fc9d15aad23783061f695576bec810089fa8f
akgande <agande@AGANDE-LAP.oradev.oraclecorp.com>
2019-07-17 23:53:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BookService } from './book/book.service';
+import { BookComponent } from './book/book.component';
+
+
+const appRoutes: Routes = [
+  { path: 'addBook', component: BookComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BookComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, HttpModule, FormsModule,  RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
